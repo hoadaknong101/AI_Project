12e6c436302fa7814d8adbfda30ccd1264e41ed9
hoaffffff <hoadaknong101@gmail.com>
2021-06-14 21:23:06 +0700

    function

--- frmNguyenLieu.Designer.cs
+++ frmNguyenLieu.Designer.cs
@@ -46,6 +46,11 @@ namespace AI_Project
             this.btnThem = new System.Windows.Forms.Button();
             this.btnHuy = new System.Windows.Forms.Button();
             this.btnNhomNL = new System.Windows.Forms.Button();
+            this.btnTaiLai = new System.Windows.Forms.Button();
+            this.txtNhapTen = new System.Windows.Forms.TextBox();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbbLoc = new System.Windows.Forms.ComboBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvNL)).BeginInit();
@@ -85,7 +90,7 @@ namespace AI_Project
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.51948F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 69.48052F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 143F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 337F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 340F));
             this.tableLayoutPanel2.Controls.Add(this.label5, 2, 1);
             this.tableLayoutPanel2.Controls.Add(this.txtTenNL, 1, 1);
             this.tableLayoutPanel2.Controls.Add(this.label4, 0, 1);
@@ -96,9 +101,10 @@ namespace AI_Project
             this.tableLayoutPanel2.Controls.Add(this.cbbNhomMon, 3, 0);
             this.tableLayoutPanel2.Location = new System.Drawing.Point(12, 65);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowCount = 3;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel2.Size = new System.Drawing.Size(960, 138);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
@@ -108,7 +114,7 @@ namespace AI_Project
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(240)))), ((int)(((byte)(225)))));
-            this.label5.Location = new System.Drawing.Point(501, 91);
+            this.label5.Location = new System.Drawing.Point(498, 76);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(99, 24);
             this.label5.TabIndex = 7;
@@ -118,7 +124,7 @@ namespace AI_Project
             // 
             this.txtTenNL.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.txtTenNL.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTenNL.Location = new System.Drawing.Point(149, 87);
+            this.txtTenNL.Location = new System.Drawing.Point(148, 72);
             this.txtTenNL.Name = "txtTenNL";
             this.txtTenNL.Size = new System.Drawing.Size(315, 32);
             this.txtTenNL.TabIndex = 6;
@@ -129,7 +135,7 @@ namespace AI_Project
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(240)))), ((int)(((byte)(225)))));
-            this.label4.Location = new System.Drawing.Point(34, 91);
+            this.label4.Location = new System.Drawing.Point(33, 76);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(78, 24);
             this.label4.TabIndex = 5;
@@ -141,7 +147,7 @@ namespace AI_Project
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(240)))), ((int)(((byte)(225)))));
-            this.label3.Location = new System.Drawing.Point(501, 22);
+            this.label3.Location = new System.Drawing.Point(498, 17);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(98, 24);
             this.label3.TabIndex = 3;
@@ -153,7 +159,7 @@ namespace AI_Project
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(240)))), ((int)(((byte)(225)))));
-            this.label2.Location = new System.Drawing.Point(36, 22);
+            this.label2.Location = new System.Drawing.Point(35, 17);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(74, 24);
             this.label2.TabIndex = 1;
@@ -163,7 +169,7 @@ namespace AI_Project
             // 
             this.txtIDNL.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.txtIDNL.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtIDNL.Location = new System.Drawing.Point(149, 18);
+            this.txtIDNL.Location = new System.Drawing.Point(148, 13);
             this.txtIDNL.Name = "txtIDNL";
             this.txtIDNL.Size = new System.Drawing.Size(315, 32);
             this.txtIDNL.TabIndex = 2;
@@ -172,7 +178,7 @@ namespace AI_Project
             // 
             this.txtSL.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.txtSL.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSL.Location = new System.Drawing.Point(625, 87);
+            this.txtSL.Location = new System.Drawing.Point(622, 72);
             this.txtSL.Name = "txtSL";
             this.txtSL.Size = new System.Drawing.Size(278, 32);
             this.txtSL.TabIndex = 8;
@@ -182,7 +188,7 @@ namespace AI_Project
             this.cbbNhomMon.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.cbbNhomMon.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbbNhomMon.FormattingEnabled = true;
-            this.cbbNhomMon.Location = new System.Drawing.Point(625, 20);
+            this.cbbNhomMon.Location = new System.Drawing.Point(622, 15);
             this.cbbNhomMon.Name = "cbbNhomMon";
             this.cbbNhomMon.Size = new System.Drawing.Size(278, 29);
             this.cbbNhomMon.TabIndex = 9;
@@ -196,7 +202,7 @@ namespace AI_Project
             this.dgvNL.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvNL.Location = new System.Drawing.Point(50, 213);
             this.dgvNL.Name = "dgvNL";
-            this.dgvNL.Size = new System.Drawing.Size(866, 311);
+            this.dgvNL.Size = new System.Drawing.Size(866, 306);
             this.dgvNL.TabIndex = 2;
             this.dgvNL.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvNL_CellClick);
             // 
@@ -205,7 +211,7 @@ namespace AI_Project
             this.btnDong.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.btnDong.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDong.ForeColor = System.Drawing.Color.Crimson;
-            this.btnDong.Location = new System.Drawing.Point(762, 534);
+            this.btnDong.Location = new System.Drawing.Point(816, 579);
             this.btnDong.Name = "btnDong";
             this.btnDong.Size = new System.Drawing.Size(100, 39);
             this.btnDong.TabIndex = 3;
@@ -218,7 +224,7 @@ namespace AI_Project
             this.btnLuu.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.btnLuu.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLuu.ForeColor = System.Drawing.Color.Crimson;
-            this.btnLuu.Location = new System.Drawing.Point(270, 534);
+            this.btnLuu.Location = new System.Drawing.Point(204, 579);
             this.btnLuu.Name = "btnLuu";
             this.btnLuu.Size = new System.Drawing.Size(100, 39);
             this.btnLuu.TabIndex = 4;
@@ -231,7 +237,7 @@ namespace AI_Project
             this.btnThem.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.btnThem.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnThem.ForeColor = System.Drawing.Color.Crimson;
-            this.btnThem.Location = new System.Drawing.Point(106, 534);
+            this.btnThem.Location = new System.Drawing.Point(51, 579);
             this.btnThem.Name = "btnThem";
             this.btnThem.Size = new System.Drawing.Size(100, 39);
             this.btnThem.TabIndex = 5;
@@ -244,7 +250,7 @@ namespace AI_Project
             this.btnHuy.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.btnHuy.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnHuy.ForeColor = System.Drawing.Color.Crimson;
-            this.btnHuy.Location = new System.Drawing.Point(434, 534);
+            this.btnHuy.Location = new System.Drawing.Point(357, 579);
             this.btnHuy.Name = "btnHuy";
             this.btnHuy.Size = new System.Drawing.Size(100, 39);
             this.btnHuy.TabIndex = 6;
@@ -257,7 +263,7 @@ namespace AI_Project
             this.btnNhomNL.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.btnNhomNL.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnNhomNL.ForeColor = System.Drawing.Color.Crimson;
-            this.btnNhomNL.Location = new System.Drawing.Point(598, 534);
+            this.btnNhomNL.Location = new System.Drawing.Point(663, 579);
             this.btnNhomNL.Name = "btnNhomNL";
             this.btnNhomNL.Size = new System.Drawing.Size(100, 39);
             this.btnNhomNL.TabIndex = 7;
@@ -265,12 +271,75 @@ namespace AI_Project
             this.btnNhomNL.UseVisualStyleBackColor = true;
             this.btnNhomNL.Click += new System.EventHandler(this.btnNhomNL_Click);
             // 
+            // btnTaiLai
+            // 
+            this.btnTaiLai.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnTaiLai.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTaiLai.ForeColor = System.Drawing.Color.Crimson;
+            this.btnTaiLai.Location = new System.Drawing.Point(510, 579);
+            this.btnTaiLai.Name = "btnTaiLai";
+            this.btnTaiLai.Size = new System.Drawing.Size(100, 39);
+            this.btnTaiLai.TabIndex = 8;
+            this.btnTaiLai.Text = "Tải lại";
+            this.btnTaiLai.UseVisualStyleBackColor = true;
+            this.btnTaiLai.Click += new System.EventHandler(this.btnTaiLai_Click);
+            // 
+            // txtNhapTen
+            // 
+            this.txtNhapTen.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtNhapTen.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNhapTen.Location = new System.Drawing.Point(144, 534);
+            this.txtNhapTen.Name = "txtNhapTen";
+            this.txtNhapTen.Size = new System.Drawing.Size(359, 32);
+            this.txtNhapTen.TabIndex = 10;
+            // 
+            // btnTim
+            // 
+            this.btnTim.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnTim.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTim.ForeColor = System.Drawing.Color.Crimson;
+            this.btnTim.Location = new System.Drawing.Point(509, 532);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(100, 35);
+            this.btnTim.TabIndex = 11;
+            this.btnTim.Text = "Tìm";
+            this.btnTim.UseVisualStyleBackColor = true;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(240)))), ((int)(((byte)(225)))));
+            this.label6.Location = new System.Drawing.Point(47, 537);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 24);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Nhập tên";
+            // 
+            // cbbLoc
+            // 
+            this.cbbLoc.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.cbbLoc.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbLoc.FormattingEnabled = true;
+            this.cbbLoc.Location = new System.Drawing.Point(634, 534);
+            this.cbbLoc.Name = "cbbLoc";
+            this.cbbLoc.Size = new System.Drawing.Size(278, 29);
+            this.cbbLoc.TabIndex = 13;
+            this.cbbLoc.TextChanged += new System.EventHandler(this.cbbLoc_TextChanged);
+            // 
             // frmNguyenLieu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(119)))), ((int)(((byte)(112)))));
-            this.ClientSize = new System.Drawing.Size(984, 585);
+            this.ClientSize = new System.Drawing.Size(984, 630);
+            this.Controls.Add(this.cbbLoc);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnTim);
+            this.Controls.Add(this.txtNhapTen);
+            this.Controls.Add(this.btnTaiLai);
             this.Controls.Add(this.btnNhomNL);
             this.Controls.Add(this.btnHuy);
             this.Controls.Add(this.btnThem);
@@ -279,7 +348,6 @@ namespace AI_Project
             this.Controls.Add(this.dgvNL);
             this.Controls.Add(this.tableLayoutPanel2);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.MaximumSize = new System.Drawing.Size(1000, 624);
             this.MinimumSize = new System.Drawing.Size(1000, 600);
             this.Name = "frmNguyenLieu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -291,6 +359,7 @@ namespace AI_Project
             this.tableLayoutPanel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvNL)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -313,5 +382,10 @@ namespace AI_Project
         private System.Windows.Forms.Button btnHuy;
         private System.Windows.Forms.ComboBox cbbNhomMon;
         private System.Windows.Forms.Button btnNhomNL;
+        private System.Windows.Forms.Button btnTaiLai;
+        private System.Windows.Forms.TextBox txtNhapTen;
+        private System.Windows.Forms.Button btnTim;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbbLoc;
     }
 }
--- frmNguyenLieu.cs
+++ frmNguyenLieu.cs
@@ -13,6 +13,12 @@ namespace AI_Project
     public partial class frmNguyenLieu : Form
     {
         private AIDB db = new AIDB();
+        private List<NGUYENLIEU> lsCanTim = new List<NGUYENLIEU>();
+        private List<NHOMNGUYENLIEU> lsNhomNL = new List<NHOMNGUYENLIEU>();
+        private List<string> lsLoc = new List<string>();
+        private List<NGUYENLIEU> lsDongVat = new List<NGUYENLIEU>(); 
+        private List<NGUYENLIEU> lsThucVat = new List<NGUYENLIEU>(); 
+        private List<NGUYENLIEU> lsGiaVi = new List<NGUYENLIEU>(); 
         public frmNguyenLieu()
         {
             InitializeComponent();
@@ -32,11 +38,36 @@ namespace AI_Project
         private void frmNguyenLieu_Load(object sender, EventArgs e)
         {
             LoadData();
+            lsNhomNL = db.NHOMNGUYENLIEUx.ToList();
+            cbbNhomMon.DataSource = lsNhomNL;
+            cbbNhomMon.DisplayMember = "TenNhom";
+            cbbNhomMon.ValueMember = "ID";
+            btnHuy.Enabled = false;
+            btnLuu.Enabled = false;
+            dgvNL.EditMode = DataGridViewEditMode.EditProgrammatically;
+            lsLoc.Add("Tất cả");
+            lsLoc.Add("Động vật");
+            lsLoc.Add("Thực vật");
+            lsLoc.Add("Gia vị");
+            cbbLoc.DataSource = lsLoc;
         }
         private void LoadData()
         {
             dgvNL.DataSource = db.NGUYENLIEUx.ToList();
             Setup();
+            var dv = from d in db.NGUYENLIEUx
+                     where d.NhomNL == 50
+                     select d;
+            var tv = from t in db.NGUYENLIEUx
+                     where t.NhomNL == 60
+                     select t;
+            var gv = from g in db.NGUYENLIEUx
+                     where g.NhomNL == 70
+                     select g;
+            lsDongVat = dv.ToList();
+            lsThucVat = tv.ToList();
+            lsGiaVi = gv.ToList();
+               
         }
         private void Setup()
         {
@@ -59,15 +90,18 @@ namespace AI_Project
                 MessageBox.Show("Dữ liệu trống", "Cảnh báo");
                 return;
             }
+            btnHuy.Enabled = true;
             txtIDNL.Text = dgvNL.CurrentRow.Cells[0].Value.ToString();
             txtTenNL.Text = dgvNL.CurrentRow.Cells[1].Value.ToString();
             txtSL.Text = dgvNL.CurrentRow.Cells[3].Value.ToString();
-            cbbNhomMon.Text = dgvNL.CurrentRow.Cells[2].Value.ToString();
+            var x = db.NHOMNGUYENLIEUx.Find(int.Parse(dgvNL.CurrentRow.Cells[2].Value.ToString()));
+            cbbNhomMon.Text = x.TenNhom;
             btnLuu.Enabled = true;
         }
 
         private void btnHuy_Click(object sender, EventArgs e)
         {
+            btnHuy.Enabled = false;
             btnLuu.Enabled = false;
             txtIDNL.Text = "";
             txtTenNL.Text = "";
@@ -77,8 +111,15 @@ namespace AI_Project
         private void btnThem_Click(object sender, EventArgs e)
         {
             //
-
-
+            var add = new NGUYENLIEU()
+            {
+                ID = int.Parse(txtIDNL.Text.Trim()),
+                TenNL = txtTenNL.Text.Trim(),
+                NhomNL = int.Parse(cbbNhomMon.Text.Trim()),
+                SoLuong = Double.Parse(txtSL.Text.Trim()),
+            };
+            db.NGUYENLIEUx.Add(add);
+            db.SaveChanges();
             //
             LoadData();
         }
@@ -86,10 +127,55 @@ namespace AI_Project
         private void btnLuu_Click(object sender, EventArgs e)
         {
             //
-
-
+            var mod = db.NGUYENLIEUx.Find(int.Parse(txtIDNL.Text.Trim()));
+            mod.SoLuong = Double.Parse(txtSL.Text.Trim());
+            db.SaveChanges();
             //
             LoadData();
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            var find = from f in db.NGUYENLIEUx
+                       where f.TenNL.StartsWith(txtNhapTen.Text)
+                       select f;
+            lsCanTim = find.ToList();
+            dgvNL.DataSource = lsCanTim;
+            Setup();
+        }
+
+        private void btnTaiLai_Click(object sender, EventArgs e)
+        {
+            dgvNL.DataSource = db.NGUYENLIEUx.ToList();
+            Setup();
+            txtNhapTen.Clear();
+            dgvNL.ClearSelection();
+            btnLuu.Enabled = false;
+            btnHuy.Enabled = false;
+        }
+
+        private void cbbLoc_TextChanged(object sender, EventArgs e)
+        {
+            if(cbbLoc.SelectedIndex == 0)
+            {
+                dgvNL.DataSource = db.NGUYENLIEUx.ToList();
+                Setup();
+            }
+            if(cbbLoc.SelectedIndex == 1)
+            {
+                dgvNL.DataSource = lsDongVat;
+                Setup();
+            }
+            if(cbbLoc.SelectedIndex == 2)
+            {
+                dgvNL.DataSource = lsThucVat;
+                Setup();
+            }
+            if(cbbLoc.SelectedIndex == 3)
+            {
+                dgvNL.DataSource = lsGiaVi;
+                Setup();
+            }
         }
     }
 }
